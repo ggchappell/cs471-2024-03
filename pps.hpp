7aa40a9296a8d38af41ff918b1d75bfc248f6370
Glenn G. Chappell <ggchappell@alaska.edu>
2024-10-10 20:17:00 -0800

    pps.hpp: make prettyPrintSquare configurable via depend. inject.

--- pps.hpp
+++ pps.hpp
@@ -1,7 +1,9 @@
 // pps.hpp
 // Glenn G. Chappell
-// 2024-10-04
+// Started: 2024-10-04
+// Updated: 2024-10-09
 // Header for prettyPrintSquare
+// There is no associated source file.
 
 #ifndef FILE_PPS_HPP_INCLUDED
 #define FILE_PPS_HPP_INCLUDED
@@ -12,34 +14,87 @@
 // For std::string, std::to_string
 
 
-inline
-int square(int n)
-{
-    return n*n;
-}
+// class Compute
+// Abstract base class for compute operations: member func doint takes
+// int, does computation, returns resulting int.
+class Compute {
+public:
+
+    virtual ~Compute() {}
+
+    virtual int doint(int n) const = 0;
+};
 
 
-inline
-std::string prettify(const std::string & ss)
-{
-    auto width = ss.size() + 4;
-    std::string result;
+// class Square
+// A compute class (derived from Compute): member func doint squares an
+// int.
+class Square : public Compute {
+public:
 
-    result = std::string(width, '*') + "\n";
-    result += "* " + ss + " *\n";
-    result += std::string(width, '*') + "\n";
+    virtual ~Square() {}
 
-    return result;
-}
+    virtual int doint(int n) const
+    {
+        return n*n;
+    }
+};
 
 
+// class Format
+// Abstract base class for format operations: member func dostr takes
+// string, does formatting, returns resulting string.
+class Format {
+public:
+
+    virtual ~Format() {}
+
+    virtual std::string dostr(const std::string & s) const = 0;
+};
+
+
+// class Box
+// A format class (derived from Format): member func dostr puts a box of
+// asterisks around // a string, with a newline at the end of each line.
+class Box : public Format {
+public:
+
+    virtual ~Box() {}
+
+    virtual std::string dostr(const std::string & s) const
+    {
+        auto width = s.size() + 4;
+        std::string result;
+
+        result = std::string(width, '*') + "\n";
+        result += "* " + s + " *\n";
+        result += std::string(width, '*') + "\n";
+
+        return result;
+    }
+};
+
+
+// prettyPrintSquare
+// Takes an int, does a compute operation, then a format operation on
+// the string form of the result, then prints to the given stream.
+// Default behavior is to square, surround with a box of asterisks, and
+// print to std output.
+//
+// So, given 5, default behavior is to print the following to standard
+// output, with a newline at the end of each line:
+// ******
+// * 25 *
+// ******
 inline
 void prettyPrintSquare(int n,
-                       std::ostream & out = std::cout)
+                       std::ostream & out = std::cout,
+                       const Compute & c = Square(),
+                       const Format & f = Box())
 {
-    auto nn = square(n);
+    auto nn = c.doint(n);
     auto ss = std::to_string(nn);
-    auto printthis = prettify(ss);
+    auto printthis = f.dostr(ss);
     out << printthis;
 }
 
